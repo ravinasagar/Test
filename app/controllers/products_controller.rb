16952091e1c94432164ebf9162d132ce2b43b839
ravina <ravinayusoft229@gmail.com>
2018-05-09 13:58:03 +0530

    Some chanmges

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,21 +3,18 @@ class ProductsController < ApplicationController
  
 
   def express
-    binding.pry
     booking =Booking.find(params[:id])
     response = EXPRESS_GATEWAY.setup_purchase(1000,
       ip: request.remote_ip,
       return_url: url_for(:action => 'complete', :id => booking.id),
       cancel_return_url: "http://localhost:3000/bookings",
     )
-    binding.pry
     redirect_to EXPRESS_GATEWAY.redirect_url_for(response.token)
   end
 
 
 
   def complete
-    binding.pry
     booking = Booking.find(params[:id])
     begin
       purchase = EXPRESS_GATEWAY.purchase(10000,
@@ -27,11 +24,9 @@ class ProductsController < ApplicationController
       )
       puts(purchase)
     rescue Exception => e
-      logger.error "Paypal error while creating payment: #{e.message}"
 
       flash[:error] = e.message
     end
-    binding.pry
 
     unless purchase.success?
       flash[:error] = "Unfortunately an error occurred:" + purchase.message
