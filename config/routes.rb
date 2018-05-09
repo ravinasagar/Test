49060da5de1928a5bbd4bcc86382cbb98c7d4e55
ravina <ravinayusoft229@gmail.com>
2018-05-09 15:56:09 +0530

    testcommit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  get 'welcomes/index'
+
+  get 'registrations/index'
+
   root 'uploaders#index'
   resources :uploaders
 
--- test/controllers/registrations_controller_test.rb
+++ test/controllers/registrations_controller_test.rb
@@ -1,48 +1,9 @@
 require 'test_helper'
 
 class RegistrationsControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @registration = registrations(:one)
-  end
-
   test "should get index" do
-    get registrations_url
+    get registrations_index_url
     assert_response :success
   end
 
-  test "should get new" do
-    get new_registration_url
-    assert_response :success
-  end
-
-  test "should create registration" do
-    assert_difference('Registration.count') do
-      post registrations_url, params: { registration: { card_expires_on: @registration.card_expires_on, card_type: @registration.card_type, cart_id: @registration.cart_id, first_name: @registration.first_name, ip_address: @registration.ip_address, last_name: @registration.last_name, new: @registration.new } }
-    end
-
-    assert_redirected_to registration_url(Registration.last)
-  end
-
-  test "should show registration" do
-    get registration_url(@registration)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_registration_url(@registration)
-    assert_response :success
-  end
-
-  test "should update registration" do
-    patch registration_url(@registration), params: { registration: { card_expires_on: @registration.card_expires_on, card_type: @registration.card_type, cart_id: @registration.cart_id, first_name: @registration.first_name, ip_address: @registration.ip_address, last_name: @registration.last_name, new: @registration.new } }
-    assert_redirected_to registration_url(@registration)
-  end
-
-  test "should destroy registration" do
-    assert_difference('Registration.count', -1) do
-      delete registration_url(@registration)
-    end
-
-    assert_redirected_to registrations_url
-  end
 end
